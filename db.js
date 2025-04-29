213e837fa647d1042ee40f89301723e5c7587955
jmetsaniemi1 <166294290+jmetsaniemi1@users.noreply.github.com>
2025-04-29 10:55:15 +0300

    Vercel connection

--- db.js
+++ db.js
@@ -2,8 +2,17 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
+  const mongoURI = process.env.MONGO_URI;
+
+  if (!mongoURI) {
+    console.error('MONGO_URI environment variable not set!');
+    process.exit(1);
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/SpaceDatabase', {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000,
     });
     console.log('MongoDB connected');
@@ -14,4 +23,5 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
 
